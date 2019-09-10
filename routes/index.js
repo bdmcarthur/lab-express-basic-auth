3fc8c590c5b83ad5e693bcb4d4208e039f1f5219
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-09-10 09:24:37 +0100

    Iteration 2

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,45 @@ router.get('/authentication/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
+router.get('/authentication/log-in', (req, res, next) => {
+  res.render('log-in');
+});
+
+router.post('/authentication/log-in', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  let auxiliaryUser;
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        throw new Error('USER_NOT_FOUND');
+      } else {
+        auxiliaryUser = user;
+        return bcrypt.compare(password, user.passwordHash);
+      }
+    })
+    .then(matches => {
+      if (!matches) {
+        throw new Error('PASSWORD_DOESNT_MATCH');
+      } else {
+        req.session.user = {
+          _id: auxiliaryUser._id
+        };
+        res.redirect('private');
+      }
+    })
+    .catch(error => {
+      console.log('There was an error signing up the user', error);
+      next(error);
+    });
+});
+
+
+
+
+
 
 const bcryptSalt     = 10;
 router.post('/authentication/sign-up', (req, res, next) => {
